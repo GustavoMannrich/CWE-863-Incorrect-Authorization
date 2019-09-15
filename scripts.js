5b19ad4a1a278d45db7eff054ec0daeb3ef3bedc
ReinerTz <llreinert@gmail.com>
2019-09-15 17:48:56 -0300

    opa fala tu

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
-document.cookie = "username=teste"
+window.onload = function(){
+    //debugger;
+    var filename = window.location.href.substring(window.location.href.lastIndexOf('/')+1);
+    if(document.cookie.username !== "teste"){
+        if (filename !== "index.html")
+            window.location.href = "index.html"
+    }else{
+        if (filename !== "pagina1.html")
+            window.location.href = "pagina1.html";
+    }
+}
+
 function setCookie(){
-    debugger;
-alert(document.cookie);
+    //debugger;
+    document.cookie = "username=teste" 
+
+    alert(document.cookie);
 }
